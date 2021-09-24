908c99a763191c115f69fb2dac8aa5bc3d43629c
Hassaan Muzammil <52124348+hassaanmuzammil@users.noreply.github.com>
2021-09-24 16:25:29 +0500

    Create allPalindromicSubstrings.py

---
+++ allPalindromicSubstrings.py
@@ -0,0 +1,48 @@
+s = 'abccba'
+
+def allPalindromicSubstring(s):
+    """
+    :type s: str
+    :rtype: list
+    """
+
+    dp = [] # 2d reference table 
+    palindromic = []
+    n = len(s)
+    count = 0
+    for i in range(n):
+        l = []
+        for j in range(n):
+            l.append(0)
+        dp.append(l)
+
+    # if start == end, then substring is palindrome (length 1 substrings)
+    for i in range(n):
+        dp[i][i] = True
+        palindromic.append(s[i])
+        
+
+    # if start + 1 == end, then check for ends (length 2 substrings) 
+    for i in range(n-1):
+        sub = s[i:i+2]
+        if sub[0] == sub[-1]:
+            dp[i][i+1] = True
+            palindromic.append(sub)
+        else:
+            dp[i][i+1] = False
+
+    # for everything else, check ends and previous diagonal entry (length > 2 substrings)
+    for g in range(2,n):
+        for i in range(n-g):
+            sub = s[i:i+g+1]
+            if sub[0] == sub[-1] and dp[i+1][i+g-1] == True:
+                dp[i][i+g] = True
+                palindromic.append(sub)
+            else:
+                dp[i][i+g] = False
+
+    return palindromic
+
+allPalindromicSubstring(s)
+
+#returns ['a', 'b', 'c', 'c', 'b', 'a', 'cc', 'bccb', 'abccba']

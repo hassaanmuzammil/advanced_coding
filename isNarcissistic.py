37b46cdcd40b4989aebbb8337801cd9f6d8c65d4
Hassaan Muzammil <52124348+hassaanmuzammil@users.noreply.github.com>
2022-01-11 12:16:33 +0500

    Update isNarcissistic.py

--- isNarcissistic.py
+++ isNarcissistic.py
@@ -1,17 +1,11 @@
-def number_of_digits(n):
-    num_digits = 0
-    while n != 0:
-        n = n // 10
-        num_digits += 1
-        
-    return num_digits
-  
 def is_narcissistic(value):
-    num_digits = number_of_digits(value)
+    
     str_value = str(value)
+    num_digits = len(str_value)
     sum = 0
+    
     for i in range(num_digits):
         sum += int(str_value[i]) ** num_digits
         
     return sum == value
-    
+ 

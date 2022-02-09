67b9c18230eb8d05856317b7d7267d4fe9df2542
Hassaan Muzammil <52124348+hassaanmuzammil@users.noreply.github.com>
2022-02-09 10:15:16 +0500

    Update getAllFilesFromFolder.py

--- getAllFilesFromFolder.py
+++ getAllFilesFromFolder.py
@@ -1,8 +1,10 @@
+import os, shutil
+
 def getAllFiles(src_folder, dest_folder, file_type):
 
     for root, dirs, files in os.walk(srcDir):
         for file in files:
             if file.endswith().lower() == '.xlsx' and not file.startswith("~$"):
-                # shutil.copy(os.path.join(root, file), os.path.join(dest, file))
+                shutil.copy(os.path.join(root, file), os.path.join(dest, file))
                 
     return None
